d4a4ce878fb5e26949d7486da9c662cfed1a1325
Marcin Michalski <mkamichalski@gmail.com>
2023-09-20 17:24:35 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function showSection(idToShow) {
+    let sections = document.querySelectorAll('section');
+        for (let i = 0; i < sections.length; i++) {
+            if (idToShow != sections[i].id) {
+            sections[i].style = `left: 150%; opacity:0`;
+        }
+    }
 
+    setTimeout(function () { document.getElementById(idToShow).style = `left: 50%; opacity:1` }, 500)
+
+}
