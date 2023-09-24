039510061bbe9c599b5d8227903d2eee9227a6bf
Marcin Michalski <mkamichalski@gmail.com>
2023-09-25 01:05:06 +0200

    0.0.2
    Major layout changes

---
+++ content.js
@@ -0,0 +1,43 @@
+
+
+const skills = {
+    name: "skills",
+    content:`
+<div class="profile-bottom">
+<div class="main-tech">
+    <div class="icon-tooltip"><img src="pics/htmlicon.svg" alt="html"><span
+            class="icon-tooltiptext">HTML</span></div>
+    <div class="icon-tooltip"><img src="pics/cssicon.svg" alt="html"><span
+            class="icon-tooltiptext">CSS</span></div>
+    <div class="icon-tooltip"><img src="pics/jsicon.svg" alt="html"><span
+            class="icon-tooltiptext">JavaScript</span></div>
+    <div class="icon-tooltip"><img src="pics/reacticon.svg" alt="html"><span
+            class="icon-tooltiptext">React.Js</span></div>
+    <div class="icon-tooltip"><img class="not-done" src="pics/tsicon.svg" alt="html"><span
+            class="icon-tooltiptext">TypeScript</span></div>
+    <div class="icon-tooltip"><img class="not-done" src="pics/sassicon.svg" alt="html"><span
+            class="icon-tooltiptext">SASS</span></div>
+    <div class="icon-tooltip"><img class="not-done" src="pics/tailicon.svg" alt="html"><span
+            class="icon-tooltiptext">Tailwhind</span></div>
+</div>
+<div class="minor-tech">
+    <div class="icon-tooltip"> <img src="pics/giticon.svg" alt="html"><span
+            class="icon-tooltiptext">GIT</span></div>
+    <div class="icon-tooltip"> <img src="pics/mysqlicon.svg" alt="html"><span
+            class="icon-tooltiptext">MySQL</span></div>
+    <div class="icon-tooltip"> <img src="pics/figmaicon.svg" alt="html"><span
+            class="icon-tooltiptext">Figma</span></div>
+    <div class="icon-tooltip"> <img src="pics/canvaicon.svg" alt="html"><span
+            class="icon-tooltiptext">Canva</span></div>
+    <div class="icon-tooltip"> <img src="pics/photoshopicon.svg" alt="html"><span
+            class="icon-tooltiptext">Photoshop</span></div>
+</div>
+</div>`
+}
+
+const projects = {
+    name: "projects",
+    content: `Project HAHAJAJ`}
+
+
+export const contentArr = [skills, projects]
--- script.js
+++ script.js
@@ -1,70 +1,35 @@
-
-// SWITCH POSITION OF TECHNOLOGY ICONS ACCORDING TO RESOLUTION
-
-let techIconsHighRes = document.querySelector('.profile-text-more')
-let techIconsLowRes = document.querySelector('.profile-bottom')
-const mediaQuery = window.matchMedia('(max-width: 945px)')
+import { contentArr } from './content.js';
+console.log(contentArr)
 
 
+const profileTextbreak = document.querySelector('.profile-text-break')
+profileTextbreak.textContent = `const person = {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+};`;
 
 
-function changeIconsPosition() {
-    if (mediaQuery.matches) {
-        techIconsLowRes.innerHTML = techIconsHighRes.innerHTML
-        techIconsHighRes.innerHTML = ""
-    } else if (!mediaQuery.matches && techIconsLowRes) {
-        techIconsHighRes.innerHTML = techIconsLowRes.innerHTML
-        techIconsLowRes.innerHTML = ""
-    }
-}
+const contentScreen = document.querySelector('section')
+const menuLink = document.querySelectorAll('.menu-link')
 
-mediaQuery.onchange = (e) => {
-    changeIconsPosition()
-}
-
-
-// SWITCH SECTION CONTENT AFTER MENU LINK CLICKED
-
-function showSection(idToShow) {
-    let sections = document.querySelectorAll('section');
-    for (let i = 0; i < sections.length; i++) {
-        if (idToShow != sections[i].id) {
-            sections[i].style = `left: 200%; opacity:0`;
-            // const element = document.getElementById(idToShow)
-            // console.log(element.id)
-            // makeContent(element)
+menuLink.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        if (!contentScreen.classList.contains(e.target.id)) {
+            for (let i = 0; i < contentArr.length; i++) {
+                if (contentArr[i].name == e.target.id) {
+                    contentScreen.style = `left: 200%; opacity:0`;
+                    console.log(e.target.id)
+                    setTimeout(function () { contentScreen.innerHTML = contentArr[i].content }, 500)
+                    contentScreen.className = e.target.id
+                    setTimeout(function () { contentScreen.style = `left: 50%; opacity:1` }, 500)
+                }
+            }
         }
-    }
-    setTimeout(function () { document.getElementById(idToShow).style = `left: 50%; opacity:1` }, 500)
-}
-
-
-
-// function makeContent(input) {
-//     input.innerHTML = `<p>www</p>`
-// }
-
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-const btn = document.querySelector('.my-btn')
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.addEventListener("click", function() {
-    modal.style.display = "block"
+    })
 })
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+
