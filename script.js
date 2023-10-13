557b690694f74cae08d76c74c2ce26b7fe5caf66
Marcin Michalski <mkamichalski@gmail.com>
2023-10-13 14:40:48 +0200

    0.2

--- script.js
+++ script.js
@@ -145,8 +145,74 @@ const observer = new IntersectionObserver((entries) => {
 });
 const techElements = document.querySelectorAll('.tech-element')
 const projectElements = document.querySelectorAll('.project-container');
-projectElements.forEach((el) => observer.observe(el));
+const contactSection = document.querySelectorAll('.contact-flex')
 
-techElements.forEach((el) => observer.observe(el));
+function observedElement(element) {
+    element.forEach((el) => observer.observe(el));
+}
 
+observedElement(projectElements);
+observedElement(contactSection);
+observedElement(techElements);
+// projectElements.forEach((el) => observer.observe(el));
+// contactSection.forEach((el) => observer.observe(el));
+// techElements.forEach((el) => observer.observe(el));
+
+
+// CARD TILT ANIMATION
+
+const tiltEffectSettings = {
+    max: 7,
+    perspective: 1000,
+    scale: 1.1,
+    speed: 1500,
+    easing: "cubic-bezier(.03,.98,.52,.99)"
+  };
+  
+  const card = document.querySelector(".card");
+
+  const mediaQuery = window.matchMedia('(max-width: 370px)')
+  if (mediaQuery.matches) {
+    tiltEffectSettings.scale = 1
+  }
+console.log(tiltEffectSettings.scale)
+
+  card.addEventListener("mouseenter", cardMouseEnter);
+  card.addEventListener("mousemove", cardMouseMove);
+  card.addEventListener("mouseleave", cardMouseLeave);
+
+
+  
+  function cardMouseEnter(event) {
+    setTransition();
+  }
+  
+  function cardMouseMove(event) {
+    const cardWidth = card.offsetWidth;
+    const cardHeight = card.offsetHeight;
+    const centerX = card.offsetLeft + cardWidth;
+    const centerY = card.offsetTop + cardHeight;
+    const mouseX = event.clientX - centerX;
+    const mouseY = event.clientY - centerY;
+    const rotateX = (-1)*(tiltEffectSettings.max*mouseY/(cardHeight/2));
+    const rotateY = (+1)*(tiltEffectSettings.max*mouseX/(cardWidth/2));
+    // const rotateX = rotateXUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : (rotateXUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateXUncapped);
+    // const rotateY = rotateYUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : (rotateYUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateYUncapped);
+  
+    card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
+                            scale3d(${tiltEffectSettings.scale}, ${tiltEffectSettings.scale}, ${tiltEffectSettings.scale})`;
+  }
+  
+  function cardMouseLeave(event) {
+    card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
+    setTransition();
+  }
+  
+  function setTransition() {
+    clearTimeout(card.transitionTimeoutId);
+    card.style.transition = `transform ${tiltEffectSettings.speed}ms ${tiltEffectSettings.easing}`;
+    card.transitionTimeoutId = setTimeout(() => {
+      card.style.transition = "";
+    }, tiltEffectSettings.speed);
+  }
 
